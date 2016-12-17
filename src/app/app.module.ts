99916c31dbcf7f04985272aa0730269fe66a3914
WillyLu326 <luzh0326@gmail.com>
2016-12-17 10:06:25 -0500

    Angular 2 Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { router } from './router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    router
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
